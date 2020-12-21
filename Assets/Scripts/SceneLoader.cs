8b68ead37b5c9ee5484449506c9fce95c7bd0c8d
DERP-DESKTOP\Nate <natebrks@gmail.com>
2020-12-21 12:37:30 +1000

    Game Over User Interface

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    int _currentSceneIndex;
+
+    private void Start()
+    {
+        _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(_currentSceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
